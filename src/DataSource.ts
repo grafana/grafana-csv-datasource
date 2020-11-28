088ef81923ee86b418799796c5098a73efa27a49
Marcus Olsson <marcus.olsson@hey.com>
2020-11-28 17:36:21 +0100

    Add support for variables

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,9 +1,19 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { MyDataSourceOptions, MyQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: MyQuery, scopedVars: ScopedVars): Record<string, any> {
+    return {
+      ...query,
+      schema: query.schema.map(({ name, type }) => ({
+        name: getTemplateSrv().replace(name, scopedVars),
+        type,
+      })),
+    };
   }
 }
