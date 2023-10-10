d9dd1f079246b7dcc12066bdb12cd8f22cf0f58a
Gareth Dawson <email@garethdawson.xyz>
2023-10-10 10:56:55 +0100

    dont track requests on dashboard reload (#264)

--- src/tracking.ts
+++ src/tracking.ts
@@ -1,8 +1,12 @@
-import { DataQueryRequest } from '@grafana/data';
+import { CoreApp, DataQueryRequest } from '@grafana/data';
 import { reportInteraction } from '@grafana/runtime';
 import { CSVQuery } from 'types';
 
 export const trackRequest = (request: DataQueryRequest<CSVQuery>) => {
+  if (request.app === CoreApp.Dashboard || request.app === CoreApp.PanelViewer) {
+    return;
+  }
+
   request.targets.forEach((target) => {
     reportInteraction('grafana_csv_query_executed', {
       app: request.app,
